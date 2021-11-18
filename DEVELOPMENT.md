ebe1e556d889f8532b62c97f4b6b9dc3c97ec0bd
Annette Poh <coding.net.p@gmail.com>
2021-11-18 16:42:50 +0000

    prepare a script to setup new database and db user

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -6,4 +6,13 @@
 
 ```
 PORT=4000
+DB_NAME=<preferred_new_database_name>
+DB_USERNAME=<preferred_new_db_username>
+DB_PASSWORD=<preferred_db_password>
+```
+
+* Run the command to create the new MySQL database and user account
+
+```
+./db-setup.sh
 ```
--- db-setup.sh
+++ db-setup.sh
@@ -1,3 +1,19 @@
 # setup required environment variables
 . .env
 
+# https://electrictoolbox.com/run-single-mysql-query-command-line/
+mysql -u root -e "CREATE USER '${DB_USERNAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USERNAME}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE ${DB_NAME}"
+
+cat << EOF > database.json
+{
+    "dev": {
+        "driver": "mysql",
+        "user": "${DB_USERNAME}",
+        "password": "${DB_PASSWORD}",
+        "database": "${DB_NAME}"
+    }
+}
+EOF
