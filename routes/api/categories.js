5d7c95770c3c788e80c25ea5d38280dcf54b2b25
Annette Poh <coding.net.p@gmail.com>
2021-11-21 03:16:38 +0000

    add logic to get/update/delete a category

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -24,6 +24,85 @@ router.get('/', async (req, res) => {
     });
 })
 
+router.get('/:category_id', async (req, res) => {
+    // fetch a category by primary key "id"
+    const categoryId = req.params.category_id
+    await Category.where({
+        'id': categoryId
+    }).fetch({
+        require: true
+    }).then(category => {
+        res.status(200).send(category.toJSON()); // convert collection to JSON
+    }).catch(_err => {
+        res.status(500).send({
+            "success": false,
+            "message": `Unable to retrieve category due to unexpected error.`
+        })
+        return;
+    });
+})
+
+router.put('/:category_id/update', async (req, res) => {
+    const category = await Category.where({
+        'id': req.params.category_id
+    }).fetch({
+        require: true
+    }).catch(_err => {
+        res.status(404).send({
+            "success": false,
+            "message": `Unable to retrieve Category ID ${req.params.category_id}. Category update failed. `
+        })
+        return;
+    });
+
+    if (category !== undefined) {
+
+        category.set('name', req.body.name);
+
+        await category.save().then(() => {
+            res.status(200).send({
+                "success": true,
+                "message": `Category ID ${req.params.category_id} updated successfully`
+            })
+        }).catch(_err => {
+            res.status(500).send({
+                "success": false,
+                "message": `Unable to update Category ID ${req.params.category_id} due to unexpected error.`
+            })
+        });
+    }
+
+})
+
+router.delete('/:category_id/delete', async (req, res) => {
+    const category = await Category.where({
+        'id': req.params.category_id
+    }).fetch({
+        require: true
+    }).catch(_err => {
+        res.status(404).send({
+            "success": false,
+            "message": `Unable to retrieve Category ID ${req.params.category_id}. Category deletion failed. `
+        })
+        return;
+    });
+
+    if (category !== undefined) {
+        await category.destroy().then(() => {
+            res.status(200).send({
+                "success": true,
+                "message": `Category ID ${req.params.category_id} deleted successfully`
+            })
+        }).catch(_err => {
+            res.status(500).send({
+                "success": false,
+                "message": `Unable to delete Category ID ${req.params.category_id} due to unexpected error.`
+            })
+        });
+    }
+
+})
+
 router.post('/create', async (req, res) => {
     const category = new Category();
     category.set('name', req.body.name);
