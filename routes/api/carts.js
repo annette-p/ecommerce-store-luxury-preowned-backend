279e5fc74795fdb661b0e6c1ea839a0080fcc2cd
Annette Poh <coding.net.p@gmail.com>
2021-11-24 17:11:46 +0000

    add logic to update cart

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -28,6 +28,59 @@ router.get('/', async (_req, res) => {
     });
 })
 
+// [checkIfAuthenticatedJWT, checkIsAdminJWT]
+router.put('/:cart_id/update', async (req, res) => {
+    const cart = await Cart.where({
+        'id': req.params.cart_id
+    }).fetch({
+        require: true,
+        withRelated:["user", "products"]
+    }).catch(_err => {
+        res.status(404).send({
+            "success": false,
+            "message": `Unable to retrieve cart id ${req.params.cart_id}. Cart update failed. `
+        })
+        return;
+    });
+
+    if (cart !== undefined) {
+
+        cart.set('user_id', req.body.user_id);
+        cart.set('date_created', new Date());
+
+        await cart.save().then(async () => {
+
+            // handle items
+            let itemsId = req.body.items.map(item => item.product_id);
+
+            console.log("itemsId: ", itemsId)
+            
+            let existinItemIds = await cart.related("products").pluck("product_id");
+
+            console.log("existinItemIds: ", existinItemIds)
+
+            // remove all the items that are not selected anymore
+            let itemsToRemove = existinItemIds.filter( product_id => itemsId.includes(product_id) === false);
+            console.log("itemsToRemove: ", itemsToRemove);
+            await cart.products().detach(itemsToRemove);
+
+            // add in all the items selected
+            await cart.products().attach(req.body.items);
+
+            res.status(200).send({
+                "success": true,
+                "message": `Cart id ${req.params.cart_id} updated successfully`
+            })
+        }).catch(_err => {
+            res.status(500).send({
+                "success": false,
+                "message": `Unable to update Cart id ${req.params.cart_id} due to unexpected error.`
+            })
+        });
+    }
+
+})
+
 // [checkIfAuthenticatedJWT, checkIsAdminJWT] --> to add after
 router.post('/create', async (req, res) => {
     const cart = new Cart();
