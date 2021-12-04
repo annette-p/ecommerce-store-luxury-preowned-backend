f33e25d12f8d6c0ebd2b0e10de6cada2b804cf7c
Annette Poh <coding.net.p@gmail.com>
2021-12-04 10:09:28 +0000

    move logic for update/delete products from api to data access layer

--- dal/products.js
+++ dal/products.js
@@ -74,12 +74,93 @@ async function getDesignerById(designerId) {
     }
 }
 
+async function updateProduct(productId, productData) {
+    const product = await getProductById(productId);
+    try {
+        if (product) {
+            if (productData.name) { product.set('name', productData.name); }
+            if (productData.designer_id) { product.set('designer_id', productData.designer_id); }
+            if (productData.category_id) { product.set('category_id', productData.category_id); }
+            if (productData.insurance_id) { product.set('insurance_id', productData.insurance_id); }
+            if (productData.quantity) { product.set('quantity', productData.quantity); }
+            
+            if (productData.retail_price) { product.set('retail_price', productData.retail_price); }
+            if (productData.selling_price) { product.set('selling_price', productData.selling_price); }
+            if (productData.description) { product.set('description', productData.description); }
+            if (productData.specifications) { product.set('specifications', productData.specifications) };
+            if (productData.condition) { product.set('condition', productData.condition); }
+            if (productData.condition_description) { product.set('condition_description', productData.condition_description); }
+            if (productData.sku) { product.set('sku', productData.sku); }
+            if (productData.authenticity) { product.set('authenticity', productData.authenticity); }
+            if (productData.product_image_1) { product.set('product_image_1', productData.product_image_1); }
+            if (productData.product_image_2) { product.set('product_image_2', productData.product_image_2); }
+            if (productData.product_gallery_1) { product.set('product_gallery_1', productData.product_gallery_1); }
+            if (productData.product_gallery_2) { product.set('product_gallery_2', productData.product_gallery_2); }
+            if (productData.product_gallery_3) { product.set('product_gallery_3', productData.product_gallery_3); }
+            if (productData.product_gallery_4) { product.set('product_gallery_4', productData.product_gallery_4); }
+            if (productData.product_gallery_5) { product.set('product_gallery_5', productData.product_gallery_5); }
+            if (productData.product_gallery_6) { product.set('product_gallery_6', productData.product_gallery_6); }
+            if (productData.product_gallery_7) { product.set('product_gallery_7', productData.product_gallery_7); }
+            if (productData.product_gallery_8) { product.set('product_gallery_8', productData.product_gallery_8); }
+            if (productData.claim_date) { product.set('claim_date', productData.claim_date); }
+            if (productData.claim_amount) { product.set('claim_amount', productData.claim_amount); }
+            if (productData.active) { product.set('authenticity', productData.active); }
+    
+            product.set('updated_at', new Date().toISOString().slice(0, 19).replace('T', ' '));
+    
+            await product.save();
+    
+            if (productData.tags) {
+                // handle tags
+                let tagIds = productData.tags.split(",");
+                let existinTagIds = await product.related("tags").pluck("id");
+    
+                // remove all the tags that are not selected anymore
+                let tagsToRemove = existinTagIds.filter( id => tagIds.includes(id) === false);
+                await product.tags().detach(tagsToRemove);
+    
+                // add in all the tags selected
+                await product.tags().attach(tagIds);
+            }
+    
+            return true;
+            
+        } else {
+            return false;
+        }
+    } catch(err) {
+        throw err
+    }
+    
+}
+
+async function deleteProduct(productId) {
+    try {
+        const product = await Product.where({
+            'id': productId
+        }).fetch({
+            require: false
+        });
+    
+        if (product) {
+            await product.destroy();
+            return true
+        } else {
+            return false;
+        }
+    } catch(err) {
+        throw err;
+    }
+}
+
 // async function createProduct(productData) {}
 
 
 module.exports = {
     // createProduct,
+    deleteProduct,
     getAllCategories, getCategoryById,
     getAllDesigners, getDesignerById,
-    getAllProducts, getProductById
+    getAllProducts, getProductById,
+    updateProduct
 }
--- routes/api/products.js
+++ routes/api/products.js
@@ -52,108 +52,56 @@ router.get('/:product_id', async (req, res) => {
     });
 })
 
+// Update a given product
 router.put('/:product_id/update', [checkIfAuthenticatedJWT, checkIsAdminJWT], async (req, res) => {
-    const product = await Product.where({
-        'id': req.params.product_id
-    }).fetch({
-        require: true,
-        withRelated:['tags']
-    }).catch(_err => {
-        res.status(404).send({
-            "success": false,
-            "message": `Unable to retrieve Product ID ${req.params.product_id}. Product update failed. `
-        })
-        return;
-    });
-
-    if (product !== undefined) {
-
-        if (req.body.name) { product.set('name', req.body.name); }
-        if (req.body.designer_id) { product.set('designer_id', req.body.designer_id); }
-        if (req.body.category_id) { product.set('category_id', req.body.category_id); }
-        if (req.body.insurance_id) { product.set('insurance_id', req.body.insurance_id); }
-        if (req.body.quantity) { product.set('quantity', req.body.quantity); }
-        
-        if (req.body.retail_price) { product.set('retail_price', req.body.retail_price); }
-        if (req.body.selling_price) { product.set('selling_price', req.body.selling_price); }
-        if (req.body.description) { product.set('description', req.body.description); }
-        if (req.body.specifications) { product.set('specifications', req.body.specifications) };
-        if (req.body.condition) { product.set('condition', req.body.condition); }
-        if (req.body.condition_description) { product.set('condition_description', req.body.condition_description); }
-        if (req.body.sku) { product.set('sku', req.body.sku); }
-        if (req.body.authenticity) { product.set('authenticity', req.body.authenticity); }
-        if (req.body.product_image_1) { product.set('product_image_1', req.body.product_image_1); }
-        if (req.body.product_image_2) { product.set('product_image_2', req.body.product_image_2); }
-        if (req.body.product_gallery_1) { product.set('product_gallery_1', req.body.product_gallery_1); }
-        if (req.body.product_gallery_2) { product.set('product_gallery_2', req.body.product_gallery_2); }
-        if (req.body.product_gallery_3) { product.set('product_gallery_3', req.body.product_gallery_3); }
-        if (req.body.product_gallery_4) { product.set('product_gallery_4', req.body.product_gallery_4); }
-        if (req.body.product_gallery_5) { product.set('product_gallery_5', req.body.product_gallery_5); }
-        if (req.body.product_gallery_6) { product.set('product_gallery_6', req.body.product_gallery_6); }
-        if (req.body.product_gallery_7) { product.set('product_gallery_7', req.body.product_gallery_7); }
-        if (req.body.product_gallery_8) { product.set('product_gallery_8', req.body.product_gallery_8); }
-        if (req.body.claim_date) { product.set('claim_date', req.body.claim_date); }
-        if (req.body.claim_amount) { product.set('claim_amount', req.body.claim_amount); }
-        if (req.body.active) { product.set('authenticity', req.body.active); }
-
-        product.set('updated_at', new Date().toISOString().slice(0, 19).replace('T', ' '));
-
-        await product.save().then(async () => {
-
-            // handle tags
-            let tagIds = req.body.tags.split(",");
-            let existinTagIds = await product.related("tags").pluck("id");
-
-            // remove all the tags that are not selected anymore
-            let tagsToRemove = existinTagIds.filter( id => tagIds.includes(id) === false);
-            await product.tags().detach(tagsToRemove);
-
-            // add in all the tags selected
-            await product.tags().attach(tagIds);
-
+    const productId = req.params.product_id
+    try {
+        let success = await productDataLayer.updateProduct(productId, req.body);
+        if (success) {
             res.status(200).send({
                 "success": true,
-                "message": `Product ID ${req.params.product_id} updated successfully`
+                "message": `Product ID ${productId} updated successfully`
             })
-        }).catch(_err => {
-            res.status(500).send({
+        } else {
+            res.status(200).send({
                 "success": false,
-                "message": `Unable to update Product ID ${req.params.product_id} due to unexpected error.`
+                "message": `Unable to update non-existent Product ID ${productId}`
             })
-        });
+        }
+    } catch(_err) {
+        console.log(_err)
+        res.status(500).send({
+            "success": false,
+            "message": `Unable to update Product ID ${productId} due to unexpected error.`
+        })
     }
-
 })
 
+// Delete a given product
 router.delete('/:product_id/delete', [checkIfAuthenticatedJWT, checkIsAdminJWT], async (req, res) => {
-    const product = await Product.where({
-        'id': req.params.product_id
-    }).fetch({
-        require: true
-    }).catch(_err => {
-        res.status(404).send({
-            "success": false,
-            "message": `Unable to retrieve Product ID ${req.params.product_id}. Product deletion failed. `
-        })
-        return;
-    });
-
-    if (product !== undefined) {
-        await product.destroy().then(() => {
+    const productId = req.params.product_id;
+    try {
+        let success = await productDataLayer.deleteProduct(productId);
+        if (success) {
             res.status(200).send({
                 "success": true,
-                "message": `Product ID ${req.params.product_id} deleted successfully`
+                "message": `Product ID ${productId} deleted successfully`
             })
-        }).catch(_err => {
-            res.status(500).send({
+        } else {
+            res.status(400).send({
                 "success": false,
-                "message": `Unable to delete Product ID ${req.params.product_id} due to unexpected error.`
+                "message": `Unable to delete non-existent Product ID ${productId}.`
             })
-        });
+        }
+    } catch(_err) {
+        res.status(500).send({
+            "success": false,
+            "message": `Unable to delete Product ID ${productId} due to unexpected error.`
+        })
     }
-
 })
 
+// Create a new product
 router.post('/create', [checkIfAuthenticatedJWT, checkIsAdminJWT], async (req, res) => {
     const product = new Product();
 
