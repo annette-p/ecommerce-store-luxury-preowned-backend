929937ce961f12ec3524372df9104b6b4953e13d
Annette Poh <coding.net.p@gmail.com>
2021-12-08 03:25:39 +0000

    in /checkout, when a Stripe checkout session initiated, fix to response with session id instead of entire session object from Stripe

--- routes/api/checkout.js
+++ routes/api/checkout.js
@@ -11,60 +11,176 @@ const {
 let jsonParser = express.json();
 
 router.post('/', [ jsonParser, checkIfAuthenticatedJWT, checkIsCustomerJWT ], async (req, res) => {
-    console.log("req.body: ", req.body)
+
+    /*
+        The request body (json) would need to be in this format:
+
+        {
+            success_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/success',
+            cancel_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/error'
+        }
+    */
     let userId = req.user.id;
     await cartServiceLayer.getCartByUser(userId).then( async (cartResult) => {
-        
 
         // Create line items
         let cart = cartResult.toJSON();
 
-        let lineItems = [];
-        let meta = [];
-        for (let item of cart.products) {
-            const lineItem = {
-                'name': item["name"],
-                'images': [ item["product_image_1"] ],
-                'amount': item["selling_price"] * 100,  // Stripe price unit is in cents
-                'quantity': item["_pivot_quantity"],
-                'currency': 'SGD'
-            }
-            lineItems.push(lineItem);
+        if (cart.products.length > 0) {
+            let lineItems = [];
+            let meta = [];
+            for (let item of cart.products) {
+                const lineItem = {
+                    'name': item["name"],
+                    'images': [ item["product_image_1"] ],
+                    'amount': item["selling_price"] * 100,  // Stripe price unit is in cents
+                    'quantity': item["_pivot_quantity"],
+                    'currency': 'SGD'
+                }
+                lineItems.push(lineItem);
 
-            // save the quantity data along with the product id
-            meta.push({
-                'product_id' : item["id"],
-                'quantity': item["_pivot_quantity"],
-                'unit_price': item["selling_price"] * 100
+                // save the quantity data along with the product id
+                meta.push({
+                    'product_id' : item["id"],
+                    'quantity': item["_pivot_quantity"],
+                    'unit_price': item["selling_price"] * 100
+                })
+            }
+
+            // create Stripe payment 
+            let metaData = JSON.stringify(meta);
+            const payment = {
+                client_reference_id: userId,
+                payment_method_types: ['card'],
+                shipping_address_collection: {
+                    allowed_countries: ['SG'],
+                },
+                line_items: lineItems,
+                success_url: req.body.success_url + '?sessionId={CHECKOUT_SESSION_ID}',
+                cancel_url: req.body.cancel_url,
+                metadata: {
+                    'orders': metaData
+                }
+            }
+
+            // register the Stripe session
+            let stripeSession = await Stripe.checkout.sessions.create(payment)
+
+            /* 
+                A sample Stripe session response
+
+                {
+                    id: 'cs_test_a10fuPYPSUMZvFyisyDlucuRRahmygTFpkTEV4M169H3kQFHPNGqbZokk0',
+                    object: 'checkout.session',
+                    after_expiration: null,
+                    allow_promotion_codes: null,
+                    amount_subtotal: 36000,
+                    amount_total: 36000,
+                    automatic_tax: { enabled: false, status: null },
+                    billing_address_collection: null,
+                    cancel_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/error',
+                    client_reference_id: '6',
+                    consent: null,
+                    consent_collection: null,
+                    currency: 'sgd',
+                    customer: null,
+                    customer_details: null,
+                    customer_email: null,
+                    expires_at: 1639019135,
+                    livemode: false,
+                    locale: null,
+                    metadata: { orders: '[{"product_id":2,"quantity":3,"unit_price":12000}]' },
+                    mode: 'payment',
+                    payment_intent: 'pi_3K4GTLDJ4PbBbIp613mW1E9r',
+                    payment_method_options: {},
+                    payment_method_types: [ 'card' ],
+                    payment_status: 'unpaid',
+                    phone_number_collection: { enabled: false },
+                    recovered_from: null,
+                    setup_intent: null,
+                    shipping: null,
+                    shipping_address_collection: { allowed_countries: [ 'SG' ] },
+                    shipping_options: [],
+                    shipping_rate: null,
+                    status: 'open',
+                    submit_type: null,
+                    subscription: null,
+                    success_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/success?sessionId={CHECKOUT_SESSION_ID}',
+                    total_details: { amount_discount: 0, amount_shipping: 0, amount_tax: 0 },
+                    url: 'https://checkout.stripe.com/pay/cs_test_a10fuPYPSUMZvFyisyDlucuRRahmygTFpkTEV4M169H3kQFHPNGqbZokk0#fidkdWxOYHwnPyd1blpxYHZxWjA0T3FKaHdBTzFVZ0dnTHUzXFBOXGB2U2pTdUwyR25TQGcxZDZ%2FY09ARjBUbj1zdGY0THU0cDJQUmFQc09LZ2diXG1CNm1dZnxgQz12SEttbkxXdlx%2FZkpNNTVnM0tvMmQ9SycpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl'
+                    }
+                    event type is checkout.session.completed
+                    stripeSession [event.data.object]:  {
+                    id: 'cs_test_a19Od08VV5Mu00Fr3Ts2SCtDW1Lx1CVfNYltMWVl2b776nIM8mKWQ9sMon',
+                    object: 'checkout.session',
+                    after_expiration: null,
+                    allow_promotion_codes: null,
+                    amount_subtotal: 36000,
+                    amount_total: 36000,
+                    automatic_tax: { enabled: false, status: null },
+                    billing_address_collection: null,
+                    cancel_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/error',
+                    client_reference_id: '6',
+                    consent: null,
+                    consent_collection: null,
+                    currency: 'sgd',
+                    customer: 'cus_Kjjy1fo6XLsXqO',
+                    customer_details: {
+                        email: 'bunny@gmail.com',
+                        phone: null,
+                        tax_exempt: 'none',
+                        tax_ids: []
+                    },
+                    customer_email: null,
+                    expires_at: 1639019134,
+                    livemode: false,
+                    locale: null,
+                    metadata: { orders: '[{"product_id":2,"quantity":3,"unit_price":12000}]' },
+                    mode: 'payment',
+                    payment_intent: 'pi_3K4GTKDJ4PbBbIp601LuhW6y',
+                    payment_method_options: {},
+                    payment_method_types: [ 'card' ],
+                    payment_status: 'paid',
+                    phone_number_collection: { enabled: false },
+                    recovered_from: null,
+                    setup_intent: null,
+                    shipping: {
+                        address: {
+                        city: '',
+                        country: 'SG',
+                        line1: 'SG',
+                        line2: null,
+                        postal_code: '12345',
+                        state: ''
+                        },
+                        name: 'Orchard'
+                    },
+                    shipping_address_collection: { allowed_countries: [ 'SG' ] },
+                    shipping_options: [],
+                    shipping_rate: null,
+                    status: 'complete',
+                    submit_type: null,
+                    subscription: null,
+                    success_url: 'https://3000-magenta-mule-ynqbqphr.ws-us21.gitpod.io/checkout/success?sessionId={CHECKOUT_SESSION_ID}',
+                    total_details: { amount_discount: 0, amount_shipping: 0, amount_tax: 0 },
+                    url: null
+                }
+            */
+
+            // Provide the Stripe session id and publishable key to the client
+            res.status(200).send({
+                "sessionId": stripeSession.id,
+                "publishableKey": process.env.STRIPE_PUBLISHABLE_KEY
+            })
+        } else {
+            // no cart items, let's not proceed
+            res.status(400).send({
+                "success": false,
+                "message": `No items in cart`
             })
         }
 
-        // create Stripe payment 
-        let metaData = JSON.stringify(meta);
-        const payment = {
-            client_reference_id: userId,
-            payment_method_types: ['card'],
-            shipping_address_collection: {
-                allowed_countries: ['SG'],
-            },
-            line_items: lineItems,
-            // success_url: process.env.STRIPE_SUCCESS_URL + '?sessionId={CHECKOUT_SESSION_ID}',
-            // cancel_url: process.env.STRIPE_ERROR_URL,
-            success_url: req.body.success_url + '?sessionId={CHECKOUT_SESSION_ID}',
-            cancel_url: req.body.cancel_url,
-            metadata: {
-                'orders': metaData
-            }
-        }
-
-        // register the Stripe session
-        let stripeSession = await Stripe.checkout.sessions.create(payment)
-
-        // Provide the Stripe session ID to the client
-        res.status(200).send({
-            "sessionId": stripeSession,
-            "publishableKey": process.env.STRIPE_PUBLISHABLE_KEY
-        })
+        
     }).catch(_err => {
         console.log(_err)
         res.status(500).send({
@@ -99,8 +215,6 @@ router.post('/process_payment', express.raw({type:'application/json'}), async (r
             let stripeSession = event.data.object;
             console.log("stripeSession [event.data.object]: ", stripeSession);
             let newOrderId = await cartServiceLayer.createOrder(stripeSession)
-            // let metadata = JSON.parse(stripeSession.metadata.orders);
-            // console.log("metadata [stripeSession.metadata.orders]: ", metadata);
             res.status(200).send({
                 'received': true,
                 "order_id": newOrderId
@@ -114,18 +228,6 @@ router.post('/process_payment', express.raw({type:'application/json'}), async (r
         })
     }
     
-})
-
-// This will move to store frontend
-router.get('/success', function(req,res){
-    console.log("Thank your order. Your order has been processed")
-    res.send("Thank your order. Your order has been processed");
-})
-
-// This will move to store frontend
-router.get('/error', function(req,res){
-    console.log("Your order has failed or has been cancelled")
-    res.send("Your order has failed or has been cancelled");
 })
 
 module.exports = router;
